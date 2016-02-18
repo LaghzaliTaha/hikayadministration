197f7dd08902332cdcda4172b3e0d85f1dafc089
LAGHZALI Taha <mrtahalz@gmail.com>
2016-02-18 10:22:17 +0100

    text

--- model/db.js
+++ model/db.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://root:root@ds031213.mongolab.com:31213/nirawa') ;
+//mongoose.connect('mongodb://root:root@ds031213.mongolab.com:31213/nirawa') ;
+mongoose.connect('mongodb://root:root@ds029117.mongolab.com:29117/stone');
